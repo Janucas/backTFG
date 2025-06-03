eb004181cecd8c121978a059fe745b73e078ff52
Juan Antonio <74763029+Janucas@users.noreply.github.com>
2025-06-03 21:55:38 +0200

    Update EquipajeDia.java

--- src/main/java/com/tfg/backend/persistance/models/EquipajeDia.java
+++ src/main/java/com/tfg/backend/persistance/models/EquipajeDia.java
@@ -20,7 +20,7 @@ public class EquipajeDia {
     private LocalDate fecha;
 
     @ManyToOne
-    @JoinColumn(name = "equipaje_dia_id")
+    @JoinColumn(name = "equipaje_id")
     private Equipaje equipaje;
 
     @OneToMany(mappedBy = "equipajeDia", cascade = CascadeType.ALL, orphanRemoval = true)
